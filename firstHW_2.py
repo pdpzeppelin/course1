d0d36bc9487ebf56b7078091cae8c7bccdb6f44c
Chris Ngo <ngo.christopher@gmail.com>
2022-08-01 17:57:34 -0500

    2nd part but couldn't finish the loop exercise. Will need to review and try later. Was a bit confsing

---
+++ firstHW_2.py
@@ -0,0 +1,71 @@
+print("Conditions")
+print(" ")
+# s = "Hey there! what should this string be?"
+s = "Strings are awesome!"
+# Length should be 20
+print("Length of s = %d" % len(s))
+
+# First occurrence of "a" should be at index 8
+print("The first occurrence of the letter a = %d" % s.index("a"))
+
+# Number of a's should be 2
+print("a occurs %d times" % s.count("a"))
+
+# Slicing the string into bits
+print("The first five characters are '%s'" % s[:5]) # Start to 5
+print("The next five characters are '%s'" % s[5:10]) # 5 to 10
+print("The thirteenth character is '%s'" % s[12]) # Just number 12
+print("The characters with odd index are '%s'" %s[1::2]) #(0-based indexing)
+print("The last five characters are '%s'" % s[-5:]) # 5th-from-last to end
+
+# Convert everything to uppercase
+print("String in uppercase: %s" % s.upper())
+
+# Convert everything to lowercase
+print("String in lowercase: %s" % s.lower())
+
+# Check how a string starts
+if s.startswith("Str"):
+    print("String starts with 'Str'. Good!")
+
+# Check how a string ends
+if s.endswith("ome!"):
+    print("String ends with 'ome!'. Good!")
+
+# Split the string into three separate strings,
+# each containing only a word
+print("Split the words of the string: %s" % s.split(" "))
+
+# This was a bit rough. Had to read to the end to understand exactly what I was supposed to do. I wasn't prepared to solve a puzzle -_-;;
+print(" ")
+print("Conditions")
+print(" ")
+# change this code --  Change the variables in the first section, so that each if statement resolves as True.
+number = 18
+second_number = 0
+first_array = [1,2,3]
+second_array = [1,2]
+
+if number > 15:
+    print("1")
+
+if first_array:
+    print("2")
+
+if len(second_array) == 2:
+    print("3")
+
+if len(first_array) + len(second_array) == 5:
+    print("4")
+
+if first_array and first_array[0] == 1:
+    print("5")
+
+if not second_number:
+    print("6")
+
+# did not get the print 6 question. is it because second number is supposed to be empty since "not" is inverted? Same with print 2? since its not asking for a specific value
+print(" ")
+print("Loops")
+print(" ")
+# Have to break here as I am confused by this less and exercise . 
