a8187302eea76aa8eda58d21183a201098cfcfb1
Chris Ngo <ngo.christopher@gmail.com>
2022-08-09 18:00:56 -0500

    previous lesson

--- game_hw.py
+++ game_hw.py
@@ -1,4 +1,7 @@
-from ast import If
+import random
+
+from copy import deepcopy
+
 # make dictionaries for user class
 # add in key for mana, rage, energy user
 # separate into separate sub-dictionaries
@@ -34,28 +37,66 @@ CLASSES = {
     }
 }
 
-ENEMY = {
-    1: {
+MOBS = [
+    {
         "name": "Archer",
         "hp": 100,
         "mana": 100,
     },
-    2: {
+    {
         "name": "Warrior",
         "hp": 120,
         "mana": 0,
     },
-    3: {
+    {
         "name": "Healer",
         "hp": 100,
         "mana": 100,
     },
-    4: {
+    {
         "name": "Spell Caster",
         "hp": 100,
         "mana": 120,
     }
+]
+
+ACTIONS = {
+    1: {
+        "name": "Heal",
+        "spells": {
+            1: {
+                "name": "Lesser Heal",
+                "value": 10,
+            },
+            2: {
+                "name": "Greater Heal",
+                "value": 19,
+            },
+        },
+    },
+    2: {
+        "name": "Attack",
+        "spells": {
+            1: {
+                "name": "Quick Attack",
+                "value": 10,
+            },
+            2: {
+                "name": "Slow Attack",
+                "value": 15,
+            },
+        }
+    }
 }
+
+
+
+def choose_action():
+    action_text = """
+        1. Heal
+        2. Attack
+        """
+    print(action_text)
 
 def choose_class():
     classes_text = """
@@ -81,12 +122,39 @@ def choose_class():
             return 0
         else:
             return chosen_class
-    
+
+
+
 # If chosen_class < 1:
 #           print(f"{chosen_class} must be larger than zero")
-#            return 0 
+#            return 0
+
 
 finished = False
 
 while finished == False:
     chosen_class = choose_class()
+
+    if chosen_class > 0:
+        character = deepcopy(CLASSES[chosen_class])
+        character_name = character['name']
+
+        print(f"You chose the {character_name} class! Be careful out there!")
+
+        alive = True
+
+        while alive == True:
+            mob = deepcopy(random.choice(MOBS))
+            mob_name = mob["name"]
+            mob_hp = mob['hp']
+            fighting = False
+
+            print(
+                f"You are fighting {mob_name} with {mob_hp} hp! Try not to die...")
+
+            ACTIONS = input(int(ACTIONS))
+            print("Choose your next {ACTIONS} ")
+
+    
+
+            alive = False
